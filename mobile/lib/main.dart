a28fa8005a6c278fd06e279b0dcd9f7134f9841c
mickeysingh3312-bit <mickeysingh3312@gmail.com>
2026-08-24 23:51:06 +0530

    Add Flutter application entrypoint

---
+++ mobile/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'screens/home_screen.dart';
+import 'screens/login_screen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final prefs = await SharedPreferences.getInstance();
+  runApp(AttendanceApp(loggedIn: prefs.getString('token') != null));
+}
+
+class AttendanceApp extends StatelessWidget {
+  final bool loggedIn;
+
+  const AttendanceApp({super.key, required this.loggedIn});
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Five Star Attendance',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xff172033)),
+          useMaterial3: true,
+        ),
+        home: loggedIn ? const HomeScreen() : const LoginScreen(),
+      );
+}
