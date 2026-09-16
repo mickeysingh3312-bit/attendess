1da6baa63ffd17d65304e4da1cf36a67e11d5347
mickeysingh3312-bit <mickeysingh3312@gmail.com>
2026-09-16 07:10:20 +0530

    Bump mobile app to v0.5.0

--- mobile/lib/config.dart
+++ mobile/lib/config.dart
@@ -1,5 +1,5 @@
 class AppConfig {
-  static const appVersion = '0.4.4';
+  static const appVersion = '0.5.0';
   static const productionApiBaseUrl =
       'https://attendees.fivestaraccess.com.au/public/api/mobile';
 
