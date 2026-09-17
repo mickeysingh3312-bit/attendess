3c56a7d9a90b6721194410a88ad510718a344c74
mickeysingh3312-bit <mickeysingh3312@gmail.com>
2026-09-17 22:05:27 +0530

    Set mobile app version to v0.5.1

--- mobile/lib/config.dart
+++ mobile/lib/config.dart
@@ -1,5 +1,5 @@
 class AppConfig {
-  static const appVersion = '0.5.0';
+  static const appVersion = '0.5.1';
   static const productionApiBaseUrl =
       'https://attendees.fivestaraccess.com.au/public/api/mobile';
 
